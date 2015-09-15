bbd2f4f9052d53554d86d68a04a463830800b04a
nekrondev <dundb@gmx.de>
2015-09-15 17:39:35 +0200

    Updated for Go 1.5.1

--- mersdk_jolla_go15_upgrade.sh
+++ mersdk_jolla_go15_upgrade.sh
@@ -18,16 +18,16 @@ GREEN='\033[0;32m'
 NOCOLORLF='\033[0m\n'
 
 # Intro
-printf "\n${GREEN}Jolla MerSDK GO 1.5 runtime upgrade by Nekron\n"
-printf "=================================================\n"
-printf "This installer will upgrade GO 1.4.2 runtime on a vanilla Jolla-1.1.6.27 SDK to GO 1.5\n"
+printf "\n${GREEN}Jolla MerSDK GO 1.5.1 runtime upgrade by Nekron\n"
+printf "===================================================\n"
+printf "This installer will upgrade GO 1.4.2 runtime on a vanilla Jolla-1.1.7.28 SDK to GO 1.5.1\n"
 printf "Before executing this script you must have GO 1.4.2 installed on your MerSDK!\n"
 printf "If not, please execute mersdk_jolla_go.sh or else upgrade will fail.\n\n"
-printf "The following modifications will be applied to GO 1.5 runtime:\n"
+printf "The following modifications will be applied to GO 1.5.1 runtime:\n"
 printf " - Apply GO 1.5 runtime patch to build go binary on MerSDK even if there are SYM errors\n"
 printf " - Apply NSIG patch to make go ARM binary work with QEMU\n\n"
 printf "The following modifications will be applied to go-qml package:\n"
-printf " - Rebuild and reinstall go-qml package for new GO 1.5 runtime for MerSDK and ARM target\n\n"
+printf " - Rebuild and reinstall go-qml package for new GO 1.5.1 runtime for MerSDK and ARM target\n\n"
 read -p "Press [ENTER] to accept the given modifications and continue installation ..."
 
 # Creating temp directory for downloads
@@ -37,19 +37,19 @@ if [ -d "~/downloads" ]; then
 fi
 cd downloads
 
-# Creating GO 1.5 runtime for MerSDK and ARM
-printf "${GREEN}Moving GO to GO1.4 needed for bootstrap GO 1.5${NOCOLORLF}\n"
+# Creating GO 1.5.1 runtime for MerSDK and ARM
+printf "${GREEN}Moving GO to GO1.4 needed for bootstrap GO 1.5.1${NOCOLORLF}\n"
 mv ~/go ~/go1.4
 cd ~/downloads
-printf "${GREEN}Downloading GO 1.5 sources ...${NOCOLORLF}\n"
-curl -O -L https://storage.googleapis.com/golang/go1.5.src.tar.gz
-tar xfvz go1.5.src.tar.gz -C ~/
+printf "${GREEN}Downloading GO 1.5.1 sources ...${NOCOLORLF}\n"
+curl -O -L https://storage.googleapis.com/golang/go1.5.1.src.tar.gz
+tar xfvz go1.5.1.src.tar.gz -C ~/
 export GOPATH=~/
 export GOROOT=~/go
-printf "${GREEN}Applying GO 1.5 MerSDK patch to fix bootstrap issues on Linaro ...${NOCOLORLF}\n"
+printf "${GREEN}Applying GO 1.5.1 MerSDK patch to fix bootstrap issues on Linaro ...${NOCOLORLF}\n"
 cd
 patch -p0 < ~/go1.5_mersdk.patch
-printf "${GREEN}Compiling GO 1.5 for MerSDK ...${NOCOLORLF}\n"
+printf "${GREEN}Compiling GO 1.5.1 for MerSDK ...${NOCOLORLF}\n"
 cd ~/go/src
 ./make.bash
 printf "${GREEN}Compiling GO for runtime ARM compilation ...${NOCOLORLF}\n"
@@ -60,9 +60,9 @@ cp ~/go/bin/gofmt ~/go/bin/gofmt_i486
 sb2 -O use-global-tmp -t SailfishOS-armv7hl ./make.bash
 cp ~/go/bin/go_i486 ~/go/bin/go
 cp ~/go/bin/gofmt_i486 ~/go/bin/gofmt
-printf "${GREEN}Go runtime 1.5 for MerSDK and ARM target prepared successfully.${NOCOLORLF}\n"
+printf "${GREEN}Go runtime 1.5.1 for MerSDK and ARM target prepared successfully.${NOCOLORLF}\n"
 
-printf "${GREEN}Updating GO QML for GO 1.5 MerSDK and ARM target ...${NOCOLORLF}\n"
+printf "${GREEN}Updating GO QML for GO 1.5.1 MerSDK and ARM target ...${NOCOLORLF}\n"
 cd ~/src/gopkg.in/qml.v1
 ~/go/bin/go install
 sb2 -O use-global-tmp -t SailfishOS-armv7hl ~/go/bin/linux_arm/go install
@@ -80,4 +80,4 @@ echo "  ]             ~ ~             | "
 echo "  |                            |  "
 echo "   |                           |  "
 echo "-----------------------------------------------------------------------"
-echo "Have fun playing around with GO 1.5, QML and your Jolla!"
+echo "Have fun playing around with GO 1.5.1, QML and your Jolla!"
